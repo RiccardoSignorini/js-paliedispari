935516e88ed5e9967cc115266f4506132f7b2875
RiccardoSignorini <riccardo.signorini1@gmail.com>
2023-03-30 17:12:26 +0200

    palindroma avanzamento

--- palindroma/assets/js/main.js
+++ palindroma/assets/js/main.js
@@ -3,3 +3,16 @@
 Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 */
+
+let word = document.querySelector('#input').value;
+
+const button = document.querySelector('#button');
+
+const risposta = document.querySelector('#risposta')
+
+button.addEventListener('click', function() {
+
+    word = document.querySelector('#input').value;
+
+    risposta.innerHTML = `<p>${word}</p>`
+})
